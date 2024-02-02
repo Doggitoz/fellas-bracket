a42ebcd5a1a3de17cc87c8b4fa039b944d5fc8aa
Coleton Wheeler <cpwheeler4@gmail.com>
2024-02-02 14:38:42 -0600

    sure

---
+++ main.js
@@ -0,0 +1,13 @@
+const h3 = document.querySelector("h3");
+const fruits = ["Apple", "Orange", "Banana", "Melon"];
+
+const fragment = new DocumentFragment();
+
+for (const fruit of fruits) {
+  const li = document.createElement("li");
+  li.textContent = fruit;
+  fragment.append(li);
+}
+
+h3.append(fragment);
+h3.append(fragment);
